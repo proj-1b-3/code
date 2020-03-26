70c2605b0ae33703e5bb4fccee98e9eae4cc3a4f
Obada-Fa <ubadafatta@gmail.com>
2020-03-26 18:50:01 +0100

    Client class

--- Client.cs
+++ Client.cs
@@ -5,5 +5,31 @@ namespace App
 	class Client
 	{
 		
+		public delegate void Command (object res, string[] args);
+
+		public static readonly Dictionary<String, Command>
+			commands = new Dictionary<String, Command>
+			{
+				{
+					"login",
+					UserRegister.LogI
+				},
+				{
+					"reserve",
+					UserRegister.LogI
+				},
+				{
+					"logout",
+					UserRegister.LogO
+				}
+			};
+
+        public static void Main(string[] args)
+        {
+			return; 
+        }
+    }
+}
+
 	}
 }
