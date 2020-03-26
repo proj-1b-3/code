1f52dc37ed552ad75c5a7704eb9dee3e21cd65cf
m-a-blommaert <maric.a.blommaert@gmail.com>
2020-03-26 18:32:41 +0100

    quick push to sync

--- ProductRegister.cs
+++ ProductRegister.cs
@@ -2,6 +2,34 @@ using System;
 
 namespace App
 {
+		class Product
+	{
+		private Int32 _Id;
+		public Int32 Id {
+			get { return _Id; }
+		}
+
+		private Int32 _Price;
+		public Int32 Price {
+			get { return _Price; }
+		}
+
+		public String Name;
+		public String Discription;
+	}
+
+	class EscapeRoom : Product
+	{
+		public String Theme;
+		public Int32 Capacity;
+		public Int32 Booked;
+
+		public Boolean IsFull()
+		{
+			return Capacity > Booked;			
+		}
+	}
+	
 	class ProductRegister
 	{
 		
