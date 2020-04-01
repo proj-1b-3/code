88197fbc9921b67922fcbe94312acc7f8f5db42d
m-a-blommaert <maric.a.blommaert@gmail.com>
2020-04-01 17:06:31 +0200

    added Try(Add/Remove)Room to the server so the client can ask to remove a room from the database

--- Room.cs
+++ Room.cs
@@ -4,7 +4,7 @@ namespace App
 
 	class Room
 	{
-		public String Name { get; set; }
+		public String RoomName { get; set; }
 		public String Theme { get; set; }
 		public String Discription { get; set; }
 		public Int32 Capacity { get; set; }
@@ -17,15 +17,6 @@ namespace App
 			Theme = theme;
 			Discription = discription;
 			Capacity = capacity;
-		}
-
-		override public String ToString()
-		{
-			return "Name: " + Name.ToString()
-				+ "\nTheme: " + Theme.ToString()
-				+ "\nDiscription: " + Discription.ToString()
-				+ "\nCapacity: " + Capacity.ToString()
-				+ "\nPrice: " + Price.ToString();
 		}
 	}
 }
