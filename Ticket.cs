b823f5476659828f7ed86b51f48b9fa411e166b2
m-a-blommaert <maric.a.blommaert@gmail.com>
2020-04-14 19:48:45 +0200

    made a few small mistake in my first push
    I didn't check the code like a dingus, now I have and now it still build
    alough it is not YET used, we'll work on that in the near future.

--- Ticket.cs
+++ Ticket.cs
@@ -1,11 +1,15 @@
 namespace App
 {
+	using System;
+	using System.Collections.Generic;
+
 	class Order
 	{
 		public Int64 OrderId {get;set;}
 		public Int64 UserId {get;set;}
 		public Int64 RoomId {get;set;}
-		public List<OrderItem> Items {get;set}
+		public List<OrderItem> Items {get;set;}
+		public DateTime DateTime {get;set;}
 
 		public Order()
 		{
@@ -13,22 +17,25 @@ namespace App
 			UserId = -1;
 			RoomId = -1;
 			Items = null;
+			DateTime = new DateTime();
 		}
 
-		public Order(Int64 roomId, List<OrderItem> items)
+		public Order(Int64 roomId, List<OrderItem> items, DateTime dateTime)
 		{
 			OrderId = -1;
 			UserId = -1;
 			RoomId = roomId;
 			Items = items;
+			DateTime = dateTime;
 		}
 
-		public Order(Int64 orderId, Int64 userId, Int64 roomId, List<OrderItem> items)
+		public Order(Int64 orderId, Int64 userId, Int64 roomId, List<OrderItem> items, DateTime dateTime)
 		{
 			OrderId = orderId;
 			UserId = userId;
 			RoomId = roomId;
 			Items = items;
+			DateTime = dateTime;
 		}
 	}
 }
